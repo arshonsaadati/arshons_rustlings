e00295711c58fe417a1e897bbe635a10c285ec26
Arshon Saadati <arshonsaadati@gmail.com>
2024-05-23 19:51:48 -0700

    50% progress yay

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -3,20 +3,23 @@
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-mod sausage_factory {
+pub mod sausage_factory {
     // Don't let anybody outside of this module see this!
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
 }
-
+mod test {
+    pub fn test() {
+        println!("testing");
+    }
+}
 fn main() {
     sausage_factory::make_sausage();
+    test::test();
 }
--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -5,8 +5,6 @@
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 struct ColorClassicStruct {
     // TODO: Something goes here
 }
@@ -23,7 +21,16 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
+        struct Color {
+            red: i32,
+            green: i32,
+            blue: i32,
+        }
+        let green = Color {
+            red: 0,
+            green: 255,
+            blue: 0,
+        };
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -33,7 +40,8 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        // let green =
+        struct Color(i32, i32, i32);
+        let green = Color(0, 255, 0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -43,7 +51,9 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit-like struct!
-        // let unit_like_struct =
+        #[derive(Debug)]
+        struct UnitLikeStruct;
+        let unit_like_struct = UnitLikeStruct;
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
